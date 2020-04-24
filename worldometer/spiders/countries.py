32d8ec4c8824f5c0f36440b848dd8230f00b9278
root <root@localhost.localdomain>
2020-04-24 22:23:43 +0530

    add country

--- worldometer/spiders/countries.py
+++ worldometer/spiders/countries.py
@@ -11,17 +11,18 @@ class CountriesSpider(scrapy.Spider):
         # title = response.xpath("//h3/text()").get()  # return title of h3 tag
         countries = response.xpath("//td/a")
         for country in countries:
-            # name = country.xpath(".//text()").get()
+            name_country = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
             # absolute_url = f"https://www.worldometer.info{link}" //manualy
-            yield response.follow(url=link, callback=self.parse_country) # url generate automaticaly
+            yield response.follow(url=link, callback=self.parse_country,meta = {'country_name':name_country}) # url generate automaticaly
     
     def parse_country(self,response):
+        country_name = response.request.meta['country_name']
         rows = response.xpath("//table[@id='usa_table_countries_today']/tbody/tr[position()>1]")
         
         for row in rows:
             city = row.xpath(".//td[1]/text()").get()
             total_cases = row.xpath(".//td[2]/text()").get()
             yield {
-                'city':city, 'total_cases':total_cases
+                'country_Name':country_name,'city':city, 'total_cases':total_cases
             }
