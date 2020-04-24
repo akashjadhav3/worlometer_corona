8f6ab6de5b6dcdea1de0a93759e597bc561a3d9f
root <root@localhost.localdomain>
2020-04-24 21:53:49 +0530

    adding click after coading

--- worldometer/spiders/countries.py
+++ worldometer/spiders/countries.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
-    start_urls = ['https://www.worldometers.info/coronavirus//']
+    start_urls = ['https://www.worldometers.info/coronavirus/']
 
     def parse(self, response):
-        title = response.xpath("//h3/text()").get()  # return title of h3 tag
+        # title = response.xpath("//h3/text()").get()  # return title of h3 tag
         countries = response.xpath("//td/a")
         for country in countries:
-            name = country.xpath(".//text()").get()
+            # name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
-
             # absolute_url = f"https://www.worldometer.info{link}" //manualy
-
-
-            yield response.follow(url=link) # url generate automaticaly
-
-        # yield{
-        #     'country_name':name,
-        #     'country_link':link
-        # }
+            yield response.follow(url=link, callback=self.parse_country) # url generate automaticaly
+    
+    def parse_country(self,response):
+        rows = response.xpath("//table[@id='usa_table_countries_today']/tbody/tr[position()>1]")
+        
+        for row in rows:
+            city = row.xpath(".//td[1]/text()").get()
+            total_cases = row.xpath(".//td[2]/text()").get()
+            yield {
+                'city':city, 'total_cases':total_cases
+            }
