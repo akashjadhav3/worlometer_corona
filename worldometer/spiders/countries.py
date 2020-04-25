083b1eb75f418c44b6c3d6185f9eea75221da442
root <root@localhost.localdomain>
2020-04-26 00:18:08 +0530

    debbuggig- VS code

--- worldometer/spiders/countries.py
+++ worldometer/spiders/countries.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import logging
-from scrapy.shell import inspect_response
+# from scrapy.shell import inspect_response
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
@@ -9,15 +9,15 @@ class CountriesSpider(scrapy.Spider):
 
     def parse(self, response):
         # title = response.xpath("//h3/text()").get()  # return title of h3 tag
-        countries = response.xpath("//td/a")
-        for country in countries:
-            name_country = country.xpath(".//text()").get()
-            link = country.xpath(".//@href").get()
-            # absolute_url = f"https://www.worldometer.info{link}" //manualy
-            yield response.follow(url='https://www.worldometers.info/coronavirus/country/us/', callback=self.parse_country,meta = {'country_name':'usa'}) # url generate automaticaly
+        # countries = response.xpath("//td/a")
+        # for country in countries:
+        #     name_country = country.xpath(".//text()").get()
+        #     link = country.xpath(".//@href").get()
+        #     # absolute_url = f"https://www.worldometer.info{link}" //manualy
+        yield response.follow(url='https://www.worldometers.info/coronavirus/country/us/', callback=self.parse_country,meta = {'country_name':'usa'}) # url generate automaticaly
     
     def parse_country(self,response):
-        inspect_response(response, self)
+        # inspect_response(response, self)
         country_name = response.request.meta['country_name']
         rows = response.xpath("//table[@id='usa_table_countries_today']/tbody/tr[position()>1]")
         
