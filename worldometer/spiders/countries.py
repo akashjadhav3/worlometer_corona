e79697bc89ae0c822d65a5f944c529b89a3aea69
root <root@localhost.localdomain>
2020-04-25 23:52:23 +0530

    Debugging- open in browser

--- worldometer/spiders/countries.py
+++ worldometer/spiders/countries.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import logging
+from scrapy.utils.response import open_in_browser
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
@@ -9,20 +10,21 @@ class CountriesSpider(scrapy.Spider):
 
     def parse(self, response):
         # title = response.xpath("//h3/text()").get()  # return title of h3 tag
-        countries = response.xpath("//td/a")
-        for country in countries:
-            name_country = country.xpath(".//text()").get()
-            link = country.xpath(".//@href").get()
+        # countries = response.xpath("//td/a")
+        # for country in countries:
+        #     name_country = country.xpath(".//text()").get()
+        #     link = country.xpath(".//@href").get()
             # absolute_url = f"https://www.worldometer.info{link}" //manualy
-            yield response.follow(url=link, callback=self.parse_country,meta = {'country_name':name_country}) # url generate automaticaly
+        yield response.follow(url='https://www.worldometers.info/coronavirus/country/us/', callback=self.parse_country,meta = {'country_name':'usa'}) # url generate automaticaly
     
     def parse_country(self,response):
-        country_name = response.request.meta['country_name']
-        rows = response.xpath("//table[@id='usa_table_countries_today']/tbody/tr[position()>1]")
+        open_in_browser(response)
+        # country_name = response.request.meta['country_name']
+        # rows = response.xpath("//table[@id='usa_table_countries_today']/tbody/tr[position()>1]")
         
-        for row in rows:
-            city = row.xpath(".//td[1]/text()").get()
-            total_cases = row.xpath(".//td[2]/text()").get()
-            yield {
-                'country_Name':country_name,'city':city, 'total_cases':total_cases
-            }
+        # for row in rows:
+        #     city = row.xpath(".//td[1]/text()").get()
+        #     total_cases = row.xpath(".//td[2]/text()").get()
+        #     yield {
+        #         'country_Name':country_name,'city':city, 'total_cases':total_cases
+        #     }
