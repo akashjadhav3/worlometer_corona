604f4eed33daab06796fa0849b2455c882a4f356
root <root@localhost.localdomain>
2020-04-26 00:22:12 +0530

    debbuggig- Vs code

--- runner.py
+++ runner.py
@@ -6,3 +6,21 @@ from worldometer.spiders.countries import CountriesSpider
 process = CrawlerProcess(settings=get_project_settings())
 process.crawl(CountriesSpider)
 process.start()
+
+
+
+
+# add  this launch.json file in vs code
+# // scrapy debugger
+#         {
+#             "name": "Crawl with scrapy",
+#             "type": "python",
+#             "request": "launch",
+#             "module": "scrapy",
+#             "cwd": "${fileDirname}",
+#             "args": [
+#                 "crawl",
+#                 "countries"
+#             ],
+#             "console": "internalConsole"
+#         }
